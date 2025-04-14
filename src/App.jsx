89ffb202be5e45b3212854411f8dc9d922092104
MS-Hriody <ridoykhancom93@gmail.com>
2025-04-14 16:56:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1>i am learnig react</h1>
+      <h1>{80+98}</h1>
+      <h1>i am learnig react</h1>
+    </div>
+  );
+};
+
+export default App;
